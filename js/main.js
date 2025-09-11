018aedb49db4ac38b61592faa8d47efb276303dd
Karen Heredia <karen.julieth.heredia@gmail.com>
2025-09-11 16:14:20 -0500

    Agregué la section de materials con sus estilos respectivos y también agregué la section de reviews con sus estilos respectivos

--- js/main.js
+++ js/main.js
@@ -23,4 +23,31 @@ $(document).ready(function () {
             }
         ]
     });
+
+    $('.slider-reviews').slick({
+    slidesToShow: 3,   // muestras 3 columnas al mismo tiempo
+    slidesToScroll: 1, // avanzas de a 1
+    infinite: true,    // loop infinito
+    speed: 300,
+    arrows: true,      // flechas
+    dots: false,        // bolitas de navegación
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+        {
+            breakpoint: 992, // tablets y pantallas medianas
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 576, // móviles
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
+    });
 });
+
+
