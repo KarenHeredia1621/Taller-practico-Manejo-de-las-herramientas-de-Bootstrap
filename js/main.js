ccbc82666db67bc3eef655da62b17fc64619bac0
Karen Heredia <karen.julieth.heredia@gmail.com>
2025-09-13 23:43:02 -0500

    Agregué una animación en una de las sections, e hice algunos ajustes en los estilos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('.slider-products').slick({
-        slidesToShow: 2 , 
+        slidesToShow: 3,  
         slidesToScroll: 1,
         infinite: true,
         arrows: true,
@@ -13,6 +13,10 @@ $(document).ready(function () {
             },
             {
                 breakpoint: 600,
+                settings: { slidesToShow: 2, slidesToScroll: 1 }
+            },
+            {
+                breakpoint: 480,
                 settings: { slidesToShow: 1, slidesToScroll: 1 }
             }
         ]
@@ -21,22 +25,25 @@ $(document).ready(function () {
 
 $(document).ready(function () {
     $('.slider-reviews').slick({
-        slidesToShow: 2,  
+        slidesToShow: 3,  
         slidesToScroll: 1,
         infinite: true,
         arrows: true,
-        dots: false,
-        autoplay: true,
+        dots: false, 
+        autoplay: false,
         responsive: [
             {
                 breakpoint: 1024,
-                settings: { slidesToShow: 2, slidesToScroll: 2 }
+                settings: { slidesToShow: 2, slidesToScroll: 1 }
             },
             {
                 breakpoint: 600,
+                settings: { slidesToShow: 2, slidesToScroll: 1 }
+            },
+            {
+                breakpoint: 480,
                 settings: { slidesToShow: 1, slidesToScroll: 1 }
             }
         ]
     });
 });
-
